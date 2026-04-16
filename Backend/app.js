f9cf714760ff466f0cfd18246a2222ecb7cf5499
Paban-1 <rpaban89@gmail.com>
2026-04-16 12:01:27 +0530

    Setup

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("working")
 })
 
+app.get("/add", (req, res) => {
+res.send("add")
+})
+
 app.listen(3000, () => {
     console.log("PORT is runing on :3000");
 })
