e8568f0adb8da9d19402620a80b0928c1004768e
Paban-1 <rpaban89@gmail.com>
2026-04-19 11:31:26 +0530

    MAJOR: Added router and handle created task

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,18 @@
 const express = require("express")
 const app = express()
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const db = require("./config/mongoose-connection")
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 
 const userRouter = require("./routes/userRouter")
 const taskRouter = require("./routes/taskRouter")
